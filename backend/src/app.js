31c3e7033c16d0ece3d1b19712df55f6f1952f32
Nanda Kishore <ellurunanda0803@gmail.com>
2026-06-27 08:50:15 +0530

    cors implemented

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.set("etag", false);
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true,
   })
 );
